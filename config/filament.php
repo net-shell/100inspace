b082ecd4c7eb59bd784e1e6dfae364590677d59a
Boyan Balkanski <balkanski@outlook.com>
2022-08-04 03:08:49 +0300

    Cleanup

--- config/filament.php
+++ config/filament.php
@@ -140,7 +140,6 @@ return [
         'path' => app_path('Filament/Widgets'),
         'register' => [
             Widgets\AccountWidget::class,
-            Widgets\FilamentInfoWidget::class,
         ],
     ],
 
@@ -198,7 +197,7 @@ return [
             'have_inline_labels' => false,
         ],
         'footer' => [
-            'should_show_logo' => true,
+            'should_show_logo' => false,
         ],
         'max_content_width' => null,
         'notifications' => [
