6ed873df96830f3c70331de0424c66820ba82145
Boyan Balkanski <balkanski@outlook.com>
2022-09-18 17:46:17 +0300

    Favicon meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="author" content="Nitrax and NetShell Bulgaria">
+        <meta name="author" content="Development: NetShell Bulgaria; Design: Nitrax;">
 
         <title>{{ env('APP_NAME') }}</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" >
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
